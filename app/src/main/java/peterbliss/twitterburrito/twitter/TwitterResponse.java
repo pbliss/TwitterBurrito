fc67a9f1aefd634fcc54d5e78d67c1376da178d6
Peter Bliss <pbliss81@gmail.com>
2015-11-08 10:58:54 -0800

    Added the actual json object to the response object

--- app/src/main/java/peterbliss/twitterburrito/twitter/TwitterResponse.java
+++ app/src/main/java/peterbliss/twitterburrito/twitter/TwitterResponse.java
@@ -1,17 +1,26 @@
 package peterbliss.twitterburrito.twitter;
 
+import org.json.JSONObject;
+
 /**
  * Created by pbliss on 11/7/2015.
  */
 public class TwitterResponse {
     private boolean status;
     private TwitterError error = null;
+    private JSONObject jsonObject;
 
     public TwitterError getError()
     {
         return error;
     }
 
+    public JSONObject getJsonObject() {
+        return jsonObject;
+    }
+    public void setJsonObject(JSONObject value) {
+        jsonObject = value;
+    }
     public void setError(TwitterError err) {
         error = err;
     }
