d4e45559cc8bd67765d3b2260564598219992523
Peter Bliss <pbliss81@gmail.com>
2015-11-08 11:01:34 -0800

    Added keyword model for holding the keywords in the system we are searching for and their results

---
+++ app/src/main/java/peterbliss/twitterburrito/models/Keyword.java
@@ -0,0 +1,39 @@
+package peterbliss.twitterburrito.models;
+
+import java.util.Date;
+import java.util.List;
+
+import io.realm.RealmObject;
+
+/**
+ * Created by pbliss on 11/8/2015.
+ */
+public class Keyword extends RealmObject{
+    private String keyword;
+    private List<Tweet> tweetList;
+    private Date lastRefresh;
+
+    public void setKeyword(String value) {
+        keyword = value;
+    }
+
+    public void setTweetList(List<Tweet> value) {
+        tweetList = value;
+    }
+
+    public void setLastRefresh(Date value) {
+        lastRefresh = value;
+    }
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public List<Tweet> getTweetList() {
+        return tweetList;
+    }
+
+    public Date getLastRefresh() {
+        return lastRefresh;
+    }
+}
