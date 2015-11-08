a35d260cffc33d23d1930301ebb971ca9c0c5aa2
Peter Bliss <pbliss81@gmail.com>
2015-11-08 11:16:51 -0800

    Added search function to the twitter class

--- app/src/main/java/peterbliss/twitterburrito/twitter/Twitter.java
+++ app/src/main/java/peterbliss/twitterburrito/twitter/Twitter.java
@@ -1,19 +1,56 @@
 package peterbliss.twitterburrito.twitter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import io.realm.Realm;
+import peterbliss.twitterburrito.models.Tweet;
+import peterbliss.twitterburrito.models.TwitterAuth;
+
 /**
  * Created by pbliss on 11/7/2015.
  */
 public class Twitter {
 
-    //api url https://api.twitter.com/1.1/
+    public final static String CONSUMER_KEY = "QhvIE54fStSbl5EFcq2Vt4b9N";
+    public final static String CONSUMER_SECRET = "QfwDgq4NnwbFD1YYTqh9hfkpGIqYc27oA8SOxeJ0xQFzbMhbgY";
+    public static TwitterAuth auth;
+
+    public interface AsyncResponse {
+        void processFinish(List<Tweet> tweets);
+    }
 
     //Ping the twitter api and search for the keyword specified
     //populate the local cache with the tweets retrieved
-    public static void search(String keyword) {
+    public static void search(String keyword, final AsyncResponse responseCallback) {
+
         //connect to api and authenticate
+        if(auth == null || auth.getToken_type().equals("INVALID")) {
+            auth = TwitterAuthenticate.authenticateConnection();
+        }
 
         //build search request
+        TwitterRequest request = new TwitterRequest(new TwitterRoute(TwitterRoute.routes.SEARCH_TWEETS));
 
-        //cache the results
+        //add required header parameters
+        request.addRequestProperty("Authorization", "Bearer " + auth.getAccess_token());
+        request.addRequestProperty("Content-Type", "application/json");
+        request.addURLParam("q", keyword);
+
+        TwitterConnection connection = new TwitterConnection();
+
+        connection.setResonseCallback(new TwitterConnection.AsyncResponse() {
+            @Override
+            public void processFinish(TwitterResponse output) {
+                //examine the results
+                if(output.getStatus()) {
+
+                    //parse the reponse into a list of tweets
+                    responseCallback.processFinish(TweetParser.parseTweets(output.getJsonObject()));
+                }
+            }
+        });
+
+        connection.execute(request);
     }
 }
