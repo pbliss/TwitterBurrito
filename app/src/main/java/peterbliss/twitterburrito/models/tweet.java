da92805dec0fdc652761245b47da77632fc7e4eb
Peter Bliss <pbliss81@gmail.com>
2015-11-07 17:00:34 -0800

    Added get set functions to the tweet and user models

--- app/src/main/java/peterbliss/twitterburrito/models/tweet.java
+++ app/src/main/java/peterbliss/twitterburrito/models/tweet.java
@@ -1,17 +1,68 @@
 package peterbliss.twitterburrito.models;
 
+import java.util.Date;
+
+import io.realm.RealmObject;
+
 /**
  * Created by pbliss on 11/7/2015.
  */
-public class tweet {
-    //this is a truncated object of the actual tweet api
+public class Tweet extends RealmObject {
+    //this is a truncated object of the actual Tweet api
     //only including the fields we will actually use
 
-    String created_at;
+    Date created_at;
     String text;
     int id;
     int favorite_count;
-    boolean favorited;
+    Boolean favorited;
     int retweet_count;
 
+    public Date getCreated_at(){
+        return created_at;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getFavorite_count() {
+        return favorite_count;
+    }
+
+    public Boolean getFavorited() {
+        return favorited;
+    }
+
+    public int getRetweet_count() {
+        return retweet_count;
+    }
+
+    private void setCreated_at(Date value) {
+        created_at = value;
+    }
+
+    public void setText(String value) {
+        text = value;
+    }
+
+    public void setId(int value) {
+        id = value;
+    }
+
+    public void setFavorite_count(int value) {
+        favorite_count = value;
+    }
+
+    public void setFavorited(Boolean value) {
+        favorited = value;
+    }
+
+    public void setRetweet_count(int value) {
+        retweet_count = value;
+    }
 }
--- app/src/main/java/peterbliss/twitterburrito/models/user.java
+++ app/src/main/java/peterbliss/twitterburrito/models/user.java
@@ -1,13 +1,55 @@
 package peterbliss.twitterburrito.models;
 
+import io.realm.RealmObject;
+
 /**
  * Created by pbliss on 11/7/2015.
  */
-public class user {
-    //truncated version of the twitter user object
+public class User extends RealmObject {
+    //truncated version of the twitter User object
     String profile_image_url;
     String name;
     String url;
     String screen_name;
     int id;
+
+    public String getProfile_image_url() {
+        return profile_image_url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getScreen_name() {
+        return screen_name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setProfile_image_url(String value) {
+        profile_image_url = value;
+    }
+
+    public void setName(String value) {
+        name = value;
+    }
+
+    public void setUrl(String value) {
+        url = value;
+    }
+
+    public void setScreen_name(String value) {
+        screen_name = value;
+    }
+
+    public void setId(int value) {
+        id = value;
+    }
 }
