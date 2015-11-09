b50a46a1ec779d88dc8aa862b0be59d3f8f99d09
Peter Bliss <pbliss81@gmail.com>
2015-11-09 14:50:16 -0800

    -Added splash screen -Added initial load items to the splash screen, get the tweets for the terms specified

--- app/src/main/java/peterbliss/twitterburrito/SplashActivity.java
+++ app/src/main/java/peterbliss/twitterburrito/SplashActivity.java
@@ -4,14 +4,16 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+import io.realm.RealmList;
+import peterbliss.twitterburrito.controllers.KeywordsController;
+import peterbliss.twitterburrito.models.TwitterAuth;
+import peterbliss.twitterburrito.twitter.TwitterAuthenticate;
 
 public class SplashActivity extends AppCompatActivity {
-
-    // SplashActivity screen timer
-    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,20 +24,40 @@ public class SplashActivity extends AppCompatActivity {
         RealmConfiguration config = new RealmConfiguration.Builder(getBaseContext()).build();
         Realm.setDefaultConfiguration(config);
 
-        new Handler().postDelayed(new Runnable() {
-            /*
-            * Showing splash screen with a timer.
-            */
+        //authenticate against twitter api here
+        TwitterAuthenticate.authenticateConnection(new TwitterAuthenticate.AsyncResponse() {
             @Override
-            public void run() {
-                //handle some initial data loading
+            public void processFinish(TwitterAuth auth) {
 
-                Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                startActivity(i);
-                // close this activity
-                finish();
+            if (auth != null) {
+                //now that its authenticated we can query up the keyword list
+                KeywordsController.keywordList = new RealmList<>();
 
+                //call the keywords setup
+                //defaulting to 5
+
+                //first keyword we will wait until it has loaded to continue
+                //this will ensure we enter the app loaded ready to use, not spinning
+                KeywordsController.addKeyword("android", new KeywordsController.AsyncResponse() {
+                    @Override
+                    public void processFinish() {
+                        Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                        startActivity(i);
+                        // close this activity
+                        finish();
+                    }
+                });
+                KeywordsController.addKeyword("angularJS");
+                KeywordsController.addKeyword("java");
+                KeywordsController.addKeyword("burrito");
+                KeywordsController.addKeyword("beer");
+            } else {
+                //error during authentication
+                Toast.makeText(getApplicationContext(), "Error authenticating twitter connection", Toast.LENGTH_SHORT);
+
+                //todo need to do something from here
             }
-        }, SPLASH_TIME_OUT); //this does assume we will be done in the timeout period
+            }
+        });
     }
 }
