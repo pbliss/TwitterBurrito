1f7b1edead550339196cfdc90986381a67a17a11
Peter Bliss <pbliss81@gmail.com>
2015-11-09 10:17:18 -0800

    Temporarily adding the authentication to the main activity will live in the splash very soon

--- app/src/main/java/peterbliss/twitterburrito/MainActivity.java
+++ app/src/main/java/peterbliss/twitterburrito/MainActivity.java
@@ -7,9 +7,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import io.realm.RealmList;
 import peterbliss.twitterburrito.controllers.KeywordsController;
+import peterbliss.twitterburrito.models.TwitterAuth;
+import peterbliss.twitterburrito.twitter.TwitterAuthenticate;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton favoritesButton;
@@ -18,6 +21,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //temporary authenticate against twitter api
+        TwitterAuthenticate.authenticateConnection(new TwitterAuthenticate.AsyncResponse() {
+            @Override
+            public void processFinish(TwitterAuth auth) {
+
+                if(auth != null) {
+
+                    //now that its authenticated we can query up the keyword list
+
+                    KeywordsController.keywordList = new RealmList<>();
+
+                    //call the keywords setup
+                    //defaulting to 5
+                    KeywordsController.addKeyword("android");
+                    KeywordsController.addKeyword("angularJS");
+                    KeywordsController.addKeyword("java");
+                    KeywordsController.addKeyword("burrito");
+                    KeywordsController.addKeyword("beer");
+                }
+                else {
+                    //error during authentication
+                    Toast.makeText(getApplicationContext(), "Error authenticating twitter connection", Toast.LENGTH_SHORT);
+
+                    //todo need to do something from here
+                }
+            }
+        });
 
         favoritesButton = (ImageButton) findViewById(R.id.openFavorites);
         favoritesButton.setOnClickListener(new View.OnClickListener() {
@@ -29,16 +60,6 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         });
-
-        KeywordsController.keywordList = new RealmList<>();
-
-        //call the keywords setup
-        //defaulting to 5
-        KeywordsController.addKeyword("android");
-        KeywordsController.addKeyword("angularJS");
-        KeywordsController.addKeyword("java");
-        KeywordsController.addKeyword("burrito");
-        KeywordsController.addKeyword("beer");
     }
 
     @Override
