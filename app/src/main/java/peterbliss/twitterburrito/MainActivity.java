06e81b6a671731250f5d18bd5a0a046cf967a060
Peter Bliss <pbliss81@gmail.com>
2015-11-09 16:58:11 -0800

    clean up of unused imports in main activity

--- app/src/main/java/peterbliss/twitterburrito/MainActivity.java
+++ app/src/main/java/peterbliss/twitterburrito/MainActivity.java
@@ -1,27 +1,12 @@
 package peterbliss.twitterburrito;
 
-import android.app.ActionBar;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
-import android.widget.Toolbar;
-
-import io.realm.RealmList;
-import peterbliss.twitterburrito.controllers.KeywordsController;
-import peterbliss.twitterburrito.models.TwitterAuth;
-import peterbliss.twitterburrito.twitter.TwitterAuthenticate;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton favoritesButton;
@@ -32,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //setup the favorites button
         favoritesButton = (ImageButton) findViewById(R.id.openFavorites);
         favoritesButton.setOnClickListener(new View.OnClickListener() {
             @Override
