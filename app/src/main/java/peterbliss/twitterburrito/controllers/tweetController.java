157476a07e576a4b512bc648985b7d4667d15b0e
Peter Bliss <pbliss81@gmail.com>
2015-11-11 12:54:41 -0800

    added tweet unicity enforcement no dupes in the results added house keeping of the cached data, removing old data non favorites from the database added twitter rate enforcement, checking on the last sliding 15 minutes for how many requests were made in that window

--- app/src/main/java/peterbliss/twitterburrito/controllers/tweetController.java
+++ app/src/main/java/peterbliss/twitterburrito/controllers/tweetController.java
@@ -1,11 +1,13 @@
 package peterbliss.twitterburrito.controllers;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import io.realm.Realm;
 import io.realm.RealmList;
-import io.realm.RealmQuery;
 import peterbliss.twitterburrito.models.Keyword;
 import peterbliss.twitterburrito.models.Tweet;
 import peterbliss.twitterburrito.twitter.Twitter;
@@ -14,23 +16,45 @@ import peterbliss.twitterburrito.twitter.Twitter;
  * Created by pbliss on 11/7/2015.
  */
 public class TweetController {
-
     //need to store the times of the requests, to keep within the rate limit
+    private static List<Calendar> requestTimes = new ArrayList<>();
 
+    private static final int SEARCHLIMIT = 180;
+    private static final int TIMEFRAME = 15;
 
     public interface AsyncResponse {
         void processFinish();
     }
 
+    //check the time frame for the limit of request made in it
+    //the time frame is a sliding window of the last X minutes
     private static Boolean checkRateLimit() {
-        //TODO
 
-        return true;
+        Calendar comp = Calendar.getInstance();
+        comp.setTime(new Date());
+        comp.add(Calendar.MINUTE, -TIMEFRAME); // removing 15 minutes
+
+        //remove any requests older than 15 mins from the list
+        for(Calendar c : requestTimes ){
+            if(c.before(comp)) {
+                requestTimes.remove(c);
+            }
+        }
+
+        if(requestTimes.size() < SEARCHLIMIT) {
+            Calendar now = Calendar.getInstance();
+            now.setTime(new Date());
+
+            requestTimes.add(now);
+            return true;
+        }
+
+        return false;
     }
 
     public static void refreshTweets(final Keyword keyword, final AsyncResponse response) {
         //check against the rate limit, only refresh if
-        //the tweets are more stale than the limit
+        //we have not gone over the limit
 
         if(checkRateLimit()) {
 
@@ -43,16 +67,23 @@ public class TweetController {
                     //begin our write transaction
                     realm.beginTransaction();
 
-                    keyword.setLoading(false);
-
                     //init the list if its null
                     if(keyword.getTweetList() == null) {
                         keyword.setTweetList(new RealmList<Tweet>());
                     }
+                    else {
+                        //trim the list of duplicates
+                        //normally would use a query but realm doesnt support that type
+                        for(Tweet existing : keyword.getTweetList()) {
+                            for (Tweet tweet : tweets) {
+                                if(tweet.getId() == existing.getId()) {
+                                    tweets.remove(tweet);
+                                }
+                            }
+                        }
+                    }
 
                     keyword.getTweetList().addAll(0, tweets);
-
-                    //keyword.setTweetList(tweets);
 
                     //commit the changes
                     realm.commitTransaction();
@@ -67,9 +98,9 @@ public class TweetController {
     }
 
     //get all favorites from a list of keywords
-    public static List<Tweet> getAllFavorites(List<Keyword> keywords) {
+    public static RealmList<Tweet> getAllFavorites(List<Keyword> keywords) {
 
-        ArrayList<Tweet> tweets = new ArrayList<>();
+        RealmList<Tweet> tweets = new RealmList<>();
 
         for(Keyword keyword : keywords) {
             tweets.addAll(getFavorites(keyword));
@@ -80,9 +111,9 @@ public class TweetController {
 
     //query the realm database and pull back a list of cached tweets that have
     //the flag of favorited
-    public static List<Tweet> getFavorites(Keyword keyword) {
+    public static RealmList<Tweet> getFavorites(Keyword keyword) {
 
-        ArrayList<Tweet> tweets = new ArrayList<>();
+        RealmList<Tweet> tweets = new RealmList<>();
 
         //avoid crashing on using a null list
         if(keyword.getTweetList() != null) {
@@ -98,8 +129,32 @@ public class TweetController {
         return tweets;
     }
 
-    //remove tweets from local database over a week old and not favorited
-    public static void cleanOldData(){
+    //remove tweets from local database over a 5 days old and not favorited
+    public static void cleanOldData(List<Keyword> keywords){
+        Realm realm = Realm.getDefaultInstance();
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date());
+        c.add(Calendar.DATE, -5); // removing 5 days
+
+        //begin our write transaction
+        realm.beginTransaction();
+
+        //realm doesnt support these complex query like this
+        //have to manually loop...
+        for(Keyword keyword : keywords) {
+            for(Tweet tweet :keyword.getTweetList())
+            {
+                Calendar created = Calendar.getInstance();
+                created.setTime(tweet.getCreated_at());
+                //remove old and non favorite tweets
+                if(created.before(c) && tweet.getFavorited() == false)  {
+                    keyword.getTweetList().remove(tweet);
+                }
+            }
+        }
+
+        realm.commitTransaction();
 
     }
 }
