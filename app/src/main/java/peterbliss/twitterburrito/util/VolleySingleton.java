9cd92438ca754d0731ebae22ff47974ffe814b8a
Peter Bliss <pbliss81@gmail.com>
2015-11-11 11:07:46 -0800

    Added Support class for volley, this supports the loading of the profile images for the users

---
+++ app/src/main/java/peterbliss/twitterburrito/util/VolleySingleton.java
@@ -0,0 +1,84 @@
+package peterbliss.twitterburrito.util;
+
+import android.graphics.Bitmap;
+import android.support.v4.util.LruCache;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import peterbliss.twitterburrito.MainActivity;
+
+/**
+ * Created by pbliss on 11/10/2015.
+ */
+
+public class VolleySingleton {
+    private static VolleySingleton mInstance = null;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+
+    private VolleySingleton(){
+        trustAllHosts();
+        mRequestQueue = Volley.newRequestQueue(MainActivity.getAppContext());
+        mImageLoader = new ImageLoader(this.mRequestQueue, new ImageLoader.ImageCache() {
+            private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(10);
+            public void putBitmap(String url, Bitmap bitmap) {
+                mCache.put(url, bitmap);
+            }
+            public Bitmap getBitmap(String url) {
+                return mCache.get(url);
+            }
+        });
+    }
+
+    public static VolleySingleton getInstance(){
+        if(mInstance == null){
+            mInstance = new VolleySingleton();
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue(){
+        return this.mRequestQueue;
+    }
+
+    public ImageLoader getImageLoader(){
+        return this.mImageLoader;
+    }
+
+    //This is to get around SSL errors. It will trust the SSL cert even though it doesn't include the rest of the chain.
+    //IT SHOULD PROBABLY NOT BE PUT INTO PRODUCTION.
+    private static void trustAllHosts() {
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws java.security.cert.CertificateException {
+            }
+
+            @Override
+            public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws java.security.cert.CertificateException {
+            }
+
+            @Override
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new java.security.cert.X509Certificate[] {};
+            }
+
+        } };
+
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection
+                    .setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
